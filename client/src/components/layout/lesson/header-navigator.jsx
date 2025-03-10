1128c99cff228eb8b4dec4e3d1835de19f6c819c
Emmanuel Fabella <efrohibire@gmail.com>
2025-03-11 01:08:12 +0800

    feat(client:lesson): dynamic markdown content

--- client/src/components/layout/lesson/header-navigator.jsx
+++ client/src/components/layout/lesson/header-navigator.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 import { Sprout, Target, ShoppingCart, ArrowUpRight } from "lucide-react";
 
-export default function Header({page}) {
+export default function Header({ page }) {
   return (
     <header className="fixed top-5 w-full px-8 flex justify-between items-center z-50">
-      <h1 className={`${page == "lesson" ? "text-black font-semibold" : ""} text-2xl`}>CapaCademy</h1>
+      <h1
+        className={`${page == "lesson" ? "text-black font-semibold" : ""} 
+        text-2xl`}
+      >
+        CapaCademy
+      </h1>
       <nav
         className="p-3 px-4 flex items-center gap-4 bg-white text-black border border-black custom-shadow-75 rounded-lg 
       *:text-sm *:flex *:gap-2 *:px-4 *:py-1 *:rounded-md "
@@ -24,7 +29,8 @@ export default function Header({page}) {
       </nav>
       <Link
         to="/dashboard/:id"
-        className="text-sm mr-8 p-2 flex bg-white border border-black text-black gap-1 custom-shadow-50 rounded-md"
+        className={`${page == "lesson" ? "" : "ml-3"} 
+        text-sm mr-8 p-2 flex bg-white border border-black text-black gap-1 custom-shadow-50 rounded-md`}
       >
         <ArrowUpRight size="20" />
         Quit
