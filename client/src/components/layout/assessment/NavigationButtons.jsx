3a346f5c31b59cff1590f6f0020db6f67a860aa4
Joselito <jr2.ayon@gmail.com>
2025-03-12 05:28:18 +0800

    style

--- client/src/components/layout/assessment/NavigationButtons.jsx
+++ client/src/components/layout/assessment/NavigationButtons.jsx
@@ -13,7 +13,7 @@ const NavigationButtons = ({ prevPage, nextPage, buttonPosition }) => {
       {prevPage && (
         <button
           onClick={prevPage}
-          className="px-25 py-2 bg-amber-500 border-2 drop-shadow-xl border-black text-black rounded-2xl hover:bg-amber-900 transition-colors my-auto w-32"
+          className="px-25 py-2 bg-amber-500 border-2 drop-shadow-xl border-black text-black rounded-2xl hover:bg-amber-900 transition-colors my-auto w-32 text-center flex items-center justify-center"
         >
           Back
         </button>
@@ -21,7 +21,7 @@ const NavigationButtons = ({ prevPage, nextPage, buttonPosition }) => {
       {nextPage && (
         <button
           onClick={nextPage}
-          className="px-25 py-2 bg-amber-500 border-2 drop-shadow-xl border-black text-black rounded-2xl hover:bg-amber-900 transition-colors my-auto w-32"
+          className="px-25 py-2 bg-amber-500 border-2 drop-shadow-xl border-black text-black rounded-2xl hover:bg-amber-900 transition-colors my-auto w-32 text-center flex items-center justify-center"
         >
           Next
         </button>
