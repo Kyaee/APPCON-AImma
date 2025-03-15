48440bed2afc9460f13c890c9c1dd6e40ec3e5ee
Emmanuel Fabella <efrohibire@gmail.com>
2025-03-15 14:45:59 +0800

    feat(routes): added account session logic & removed: duplicate routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,85 +1,93 @@
+import { ThemeProvider } from "./components/features/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "./config/authContext";
 
-import TestPage from "./routes/test-page";
-/*
-  WEB PAGES 
-  - Check the `Routes` folder for more pages 
-*/
-import Landing from "./routes/landing"
-import IntroShowcase from "./routes/showcase"
-// INTRO ASSESSMENT
-import CombinedAssessment from "./routes/user-assessment/combined-assessment";
-import DailyGoal from './routes/user-assessment/daily-goal'
-import Goals from './routes/user-assessment/goals'
-import Proficiency from './routes/user-assessment/proficiency'
-import Questions from './routes/user-assessment/questions'
-import Results from './routes/user-assessment/results'
-// GENERAL PAGES
-import Dashboard from "./routes/dashboard"
-import Profile from "./routes/profile"
-import Shop from "./routes/shop"
-import Lesson from "./routes/lesson"
-import LessonAssessment from "./routes/lesson-assessment/lesson-assessment"
-import JobOpportunities from "./routes/job-opportunities/job-opportunities"
-import NotFound from "./routes/NotFound"
+/*------------------------------------------
+WEB PAGES 
+- Check the `Routes` folder for more pages 
+------------------------------------------*/
+
+import Landing from "./routes/landing";
 // AUTHENTICATION
 import LoginPage from "./routes/auth/login";
 import RegisterPage from "./routes/auth/register";
+// INTRO ASSESSMENT
+import IntroShowcase from "./routes/showcase";
+import CombinedAssessment from "./routes/user-assessment/combined-assessment";
+import DailyGoal from "./routes/user-assessment/daily-goal";
+import Goals from "./routes/user-assessment/goals";
+import Proficiency from "./routes/user-assessment/proficiency";
+import Questions from "./routes/user-assessment/questions";
+import Results from "./routes/user-assessment/results";
+// GENERAL PAGES
+import Dashboard from "./routes/dashboard";
+import Profile from "./routes/profile";
+import Shop from "./routes/shop";
+import Lesson from "./routes/lesson";
+import LessonAssessment from "./routes/lesson-assessment/lesson-assessment";
+import JobOpportunities from "./routes/job-opportunities/job-opportunities";
+import NotFound from "./routes/NotFound";
+// TESTING PAGE
+import TestPage from "./routes/test-page";
 
-// Theme provider
-import { ThemeProvider } from "./components/features/theme-provider";
+// This is not a page @Jun, but a component
 import CourseSelect from "./components/features/course-select";
 
 function App() {
+  const { session } = useAuth();
+  const [isAssessed, setAssessed] = useState(true);
+
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <BrowserRouter>
           <Routes>
-            <Route path="/auth/login" element={<LoginPage />} />
-            <Route path="/auth/register" element={<RegisterPage />} />
 
+            {/* GENERAL ROUTES */}
             <Route path="/" element={<Landing />} />
-            
-            <Route path="/start/showcase" element={<IntroShowcase />} />
-
-            {/* 
-              To access dynamic routes like /:id/profile,
-              you can access the page by going to /1/profile
-              or /2/dashboard or /3/shop.
-            */}
-  
-            <Route path="/dashboard/:id" element={<Dashboard />} />
-            <Route path="/profile/:id" element={<Profile />} />
-            <Route path="/shop/:id" element={<Shop />} />
-            <Route
-              path="/job-opportunities/:id"
-              element={<JobOpportunities />}
-            />
-            <Route path="/:id/lesson/:id" element={<Lesson />} />
-            <Route
-              path="/:id/lesson/assessment/:id"
-              element={<LessonAssessment />}
-            />
-            
-            <Route path="/assessment/daily-goal" element={<DailyGoal />} />
-            <Route path="/assessment/goals" element={<Goals />} />
-            <Route path="/assessment/proficiency" element={<Proficiency />} />
-            <Route path="/assessment/questions" element={<Questions />} />
-            <Route path="/assessment/results" element={<Results />} />
-            <Route path="/assessment" element={<CombinedAssessment />} />
-            <Route path="/select" element={<CourseSelect />} />   
-
-            {/* To access dynamic routes like /:id/profile, you can access the page by going to /1/profile  or /2/dashboard or /3/shop. */}
-            <Route path="/dashboard/:id" element={<Dashboard />} />
-            <Route path="/profile/:id" element={<Profile />} />
-            <Route path="/job-opportunities/:id" element={<JobOpportunities />} />
-            <Route path="/shop/:id" element={<Shop />} />
-            <Route path="/:id/lesson/:id" element={<Lesson />} />
-            <Route path="/:id/lesson/assessment/:id" element={<LessonAssessment />} />
-            <Route path="/testing" element={<TestPage />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/testing" element={<TestPage />} />
 
+            {!session ? (
+              // IF USER IS NOT LOGGED-IN
+              <>
+                <Route path="/auth/login" element={<LoginPage />} />
+                <Route path="/auth/register" element={<RegisterPage />} />
+              </>
+            ) : !isAssessed ? (
+              // IF USER IS LOGGED-IN, AND NOT ASSESSED
+              <>
+                <Route path="/start/showcase" element={<IntroShowcase />} />
+                <Route path="/assessment/daily-goal" element={<DailyGoal />} />
+                <Route path="/assessment/goals" element={<Goals />} />
+                <Route
+                  path="/assessment/proficiency"
+                  element={<Proficiency />}
+                />
+                <Route path="/assessment/questions" element={<Questions />} />
+                <Route path="/assessment/results" element={<Results />} />
+                <Route path="/assessment" element={<CombinedAssessment />} />
+                <Route path="/select" element={<CourseSelect />} />
+              </>
+            ) : (
+              // IF USER IS LOGGED-IN AND ASSESSED
+              <>
+                {/* To access dynamic routes like /:id/profile, you can access the page by going to /1/profile  or /2/dashboard or /3/shop. */}
+                <Route path="/dashboard/:id" element={<Dashboard />} />
+                <Route path="/profile/:id" element={<Profile />} />
+                <Route path="/shop/:id" element={<Shop />} />
+                <Route
+                  path="/job-opportunities/:id"
+                  element={<JobOpportunities />}
+                />
+                <Route path="/:id/lesson/:id" element={<Lesson />} />
+                <Route
+                  path="/:id/lesson/assessment/:id"
+                  element={<LessonAssessment />}
+                />
+              </>
+            )}
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
