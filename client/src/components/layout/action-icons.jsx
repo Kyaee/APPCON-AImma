a3dec295237c015f2243144127d549295c953067
Emmanuel Fabella <efrohibire@gmail.com>
2025-04-01 02:54:00 +0800

    feat: settings log-out and options for language change

--- client/src/components/layout/action-icons.jsx
+++ client/src/components/layout/action-icons.jsx
@@ -14,10 +14,24 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Bell, Settings } from "lucide-react";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { languages } from "@/lib/languages";
+import SelectLanguage from "../features/select-language";
+import { useAuth } from "@/config/authContext";
 
 export default function IconButton() {
+  const { signOut } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
+  const logOut = () => {
+    signOut()
+      .then(() => {
+        console.log("User signed out successfully.");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error.message);
+      });
+  }
 
   return (
     <div className="relative flex items-center justify-center gap-4 hover:opacity-80 transition-opacity text-foreground *:size-6 z-50">
@@ -35,13 +49,45 @@ export default function IconButton() {
         </AlertDialogTrigger>
         <AlertDialogContent className="bg-background">
           <AlertDialogHeader>
-            <AlertDialogTitle className="text-foreground">Settings</AlertDialogTitle>
-            <AlertDialogDescription className="flex gap-2 items-center">
-              <ModeToggle className="border-2 border-foreground"/>
-              <p className="text-foreground">
-                Change the theme of the app<br/>
-                Guide how. <a className="text-blue-500" href="https://ui.shadcn.com/docs/theming">https://ui.shadcn.com/docs/theming</a>
-              </p>
+            <AlertDialogTitle className="text-foreground">
+              Settings
+            </AlertDialogTitle>
+            <AlertDialogDescription className="space-y-5 ">
+              <div className="flex items-center gap-2">
+                <ModeToggle className="border-2 border-foreground" />
+                <div className="text-foreground">
+                  Change the theme of the app
+                  <br />
+                  Guide how.{" "}
+                  <a
+                    className="text-blue-500"
+                    href="https://ui.shadcn.com/docs/theming"
+                  >
+                    https://ui.shadcn.com/docs/theming
+                  </a>
+                </div>
+              </div>
+              <div>
+                <div className="text-foreground">
+                  Change the language of the app
+                  <br />
+                  <br />
+                  <SelectLanguage
+                    className="text-foreground"
+                    options={languages}
+                    defaultValue="English"
+                    onChange={(e) => console.log(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <Button onClick={logOut} className="text-background bg-foreground max-w-30">
+                  Log-out
+                </Button>
+                <Button variant="outline" className="max-w-30">
+                  Support Us
+                </Button>
+              </div>
             </AlertDialogDescription>
           </AlertDialogHeader>
           <Separator
@@ -49,8 +95,12 @@ export default function IconButton() {
             className="border border-foreground"
           />
           <AlertDialogFooter>
-            <AlertDialogCancel className="text-foreground bg-background hover:bg-accent">Cancel</AlertDialogCancel>
-            <AlertDialogAction className="text-background bg-foreground hover:bg-accent">Continue</AlertDialogAction>
+            <AlertDialogCancel className="text-foreground bg-background hover:bg-accent">
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction className="text-background bg-foreground hover:bg-accent">
+              Continue
+            </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
